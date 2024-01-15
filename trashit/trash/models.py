94336f7ff7228c62bf42b7504722fc12fe6b048a
racine <cactegra@protonmail.com>
2024-01-15 13:50:53 +0100

    set object id

--- trashit/trash/models.py
+++ trashit/trash/models.py
@@ -5,6 +5,7 @@ from datapop.models import Pointfield
 
 
 class TrashSpecificities(models.Model):
+    id = models.AutoField(primary_key=True, editable=False)
     point_field = models.OneToOneField(Pointfield, on_delete=models.CASCADE)
     trash_type = models.CharField(max_length=250, null=True, blank=True)
     reported = models.BooleanField(default=False)
