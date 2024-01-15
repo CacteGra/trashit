470f099746bf852a16a77ae1103ba8a5a3486894
racine <cactegra@protonmail.com>
2024-01-15 17:00:12 +0100

    js not function good js

--- trashit/datapop/models.py
+++ trashit/datapop/models.py
@@ -455,7 +455,7 @@ class OperatedField(ClusterableModel):
 
 
 class RegisterAPI(ClusterableModel):
-    id = models.AutoField(primary_key=True, editable=False)
+    ps4
     api_title = models.TextField(max_length=100)
     city = models.CharField(max_length=250, null=True, blank=True)
     country = models.CharField(max_length=250, null=True, blank=True)
