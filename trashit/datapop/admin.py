3d561d5d3920241f2c9db84b1a4f30c3807bf437
racine <cactegra@protonmail.com>
2024-02-05 17:13:47 +0100

    missing import

--- trashit/datapop/admin.py
+++ trashit/datapop/admin.py
@@ -1,6 +1,7 @@
+from django.contrib import admin
 from django.contrib.gis.admin import OSMGeoAdmin
 from .models import Pointfield
 
-@admin.register(Shop)
+@admin.register(Issue)
 class IssueAdmin(OSMGeoAdmin):
     list_display = ('trashpecificities', 'o_field')
--- trashit/trash/admin.py
+++ trashit/trash/admin.py
@@ -1,6 +1,7 @@
+from django.contrib import admin
 from django.contrib.gis.admin import OSMGeoAdmin
 from .models import TrashSpecificities
 
-@admin.register(Shop)
-class IssueAdmin(OSMGeoAdmin):
+@admin.register(TrashIssue)
+class TrashIssueAdmin(OSMGeoAdmin):
     list_display = ('trash_type', 'point_field')
