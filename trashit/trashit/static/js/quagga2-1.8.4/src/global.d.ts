46611e1fa7341b5e3e8dc36b439ac594cfd93533
cactegra <cactegra@protonmail.com>
2024-05-21 14:50:47 +0200

    npm quagga and good import

--- trashit/trashit/static/js/quagga2-1.8.4/src/global.d.ts
+++
@@ -1,7 +0,0 @@
-interface QuaggaBuildEnvironment {
-    development?: boolean;
-    node?: boolean;
-}
-
-// injected by the build system
-declare const ENV: QuaggaBuildEnvironment;
--- trashit/trashit/static/js/quagga2-1.8.4/src/quagga/Exception.ts
+++
@@ -1,9 +0,0 @@
-export default class Exception extends Error {
-    code?: number;
-
-    constructor(m: string, code?: number) {
-        super(m);
-        this.code = code;
-        Object.setPrototypeOf(this, Exception.prototype);
-    }
-}
