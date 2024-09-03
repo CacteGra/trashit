aed64ab654d4f0c066c64aac969fb9cbb2493ab9
cactegra <cactegra@protonmail.com>
2024-09-03 15:26:02 +0200

    trying parental

--- trashit/datapop/models.py
+++ trashit/datapop/models.py
@@ -349,7 +349,8 @@ class Chosen(models.Model):
 
 class RegisterAPIChosen(Orderable, models.Model):
     register_api = ParentalKey("RegisterAPI", related_name="the_api", on_delete=models.CASCADE, null=True, blank=True)
-    register_api_foreign = models.ForeignKey('RegisterAPI', related_name="first_api", on_delete=models.CASCADE, null=True, blank=True)
+    register_api_foreign = ParentalKey('RegisterAPI', related_name="first_api", on_delete=models.CASCADE, null=True, blank=True)
+    # register_api_foreign = models.ForeignKey('RegisterAPI', related_name="first_api", on_delete=models.CASCADE, null=True, blank=True)
     # operated_select = ParentalManyToManyField("OperatedField", related_name="the_operated", blank=True)
     # operated_select_foreign = models.ForeignKey('OperatedField', on_delete=models.CASCADE, null=True, blank=True)
     hierarchy = models.PositiveIntegerField(null=True, blank=True)
