1771b719d30b962a89b53bd75ea61d6f12bfb8a4
racine <cactegra@protonmail.com>
2026-02-24 14:24:09 +0000

    scan wrapper results not displaying

--- trashit/trash/views.py
+++ trashit/trash/views.py
@@ -109,12 +109,8 @@ class ScanWrapper(LoginRequiredMixin, ListView):
         from .all_functions import openfoodfacts_api
         code = int(request.GET['code'])
         pk = openfoodfacts_api.main(code)
-        if pk:
-            wrapper = Wrapper.objects.get(pk=pk)
-            html = render_to_string('trash/packaging-bin.html', {'packagings': wrapper.packaging.all(), 'wrapper': True}, request=request)
-            print(html)
-        else:
-            html = render_to_string('trash/packaging-bin.html', {'packagings': None, 'wrapper': False}, request=request)
+        wrapper = Wrapper.objects.get(pk=pk)
+        html = render_to_string('trash/packaging-bin.html', {'packagings': wrapper.packaging.all(), 'wrapper': True}, request=request)
         return JsonResponse([{'html': html, }], safe=False)
 
 class IssueChooseView(ChooseView):
