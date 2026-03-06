0d7e47a28356990c70f7a83b4feb9f732147ac46
racine <cactegra@protonmail.com>
2026-03-06 08:26:58 +0000

    cleaning up commented and commeting

--- trashit/datapop/wagtail_hooks.py
+++ trashit/datapop/wagtail_hooks.py
@@ -89,13 +89,6 @@ def first_connection(request, instance):
         t.save()
     return True
 
-# @hooks.register('before_create_snippet')
-# def first_connection(request, register_api):
-#     r = RegisterAPI.objects.get(pk=register_api.pk)
-#     l = unique_get_data.main(r.api_endpoint)
-#     one_list_item.main(l, r.pk)
-#     return True
-
 @hooks.register('after_edit_snippet')
 def first_connection(request, instance):
     from OSMPythonTools.nominatim import Nominatim
@@ -179,10 +172,10 @@ class RegisterAPITemplate(SnippetViewSet):
         FieldPanel('results'),
         FieldPanel('until_line'),
         FieldPanel('sleep'),
-        # MultipleChooserPanel("the_api",
-        #     chooser_field_name="the_chosen",
-        #     label="API Key(s)", min_num=0)
-        MultipleChooserPanel('the_api', chooser_field_name='the_chosen', label="API Key(s)", min_num=0),
+        # Display related API keys to select from
+        MultipleChooserPanel("the_api",
+            chooser_field_name="the_chosen",
+            label="API Key(s)", min_num=0)
     ]
 
 class RegisterAPIChosenIndex(IndexView):
@@ -316,14 +309,6 @@ class OperatedTemplate(SnippetViewSet):
     model = OperatedField
 
     panels = [
-        # MultipleChooserPanel("the_operated",
-        #     chooser_field_name="chosen",
-        #     label="API Key(s)", min_num=0),
-        # InlinePanel("chosen_select"),
-        # FieldPanel("register_api_chosen", widget=CustomCheckboxSelectMultiple),
-        # FieldPanel("register_api_chosen", widget=CustomMultipleChoiceField),
-        # FieldPanel("register_api_chosen", widget=OperatedChooserWidget),
-        # FieldPanel("register_api_chosen", widget=forms.CheckboxSelectMultiple(choices=[RegisterAPIChosen.objects.filter(field_type__isnull=False)])),
         TypedOnlyPanel("register_api_chosen", widget_class=CheckboxSelectMultiple),
         FieldPanel('field_type'),
         FieldPanel('field_name'),
@@ -404,11 +389,7 @@ class TheTypeTemplate(SnippetViewSet):
     model = TheType
     panels = [
         FieldPanel('the_type'),
-        #FieldPanel('osm_type', widget=CheckboxSelectMultiple),
         OSMType("osm_type", widget_class=CheckboxSelectMultiple),
-        #MultipleChooserPanel(
-        #    'related_osm', label="Related OSM types", chooser_field_name="OSM type(s)"
-        #),
         FieldPanel('icon'),
         InlinePanel('related_the_type'),
     ]
