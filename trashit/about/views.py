9aa7fb398423500795911ff890df081ba180fe23
cactegra_soyo <cactegra@protonmail.com>
2026-04-29 16:44:40 +0200

    fr trans

--- trashit/about/views.py
+++ trashit/about/views.py
@@ -6,4 +6,34 @@ class About(TemplateView):
     template_name = 'about/index.html'
 
 class PrivacyPolicy(TemplateView):
-    template_name = 'about/privacy-policy.html'
+    def get_template_names(self):
+        # Your logic to determine the template name
+        template_name = self.determine_template_name()
+        return [template_name]
+
+    def determine_template_name(self):
+        user_languages = self.request.META.get('HTTP_ACCEPT_LANGUAGE', 'en')
+
+        languages = []
+        if user_languages:
+            for lang in user_languages.split(','):
+                lang_code = lang.split(';')[0].strip()
+                languages.append(lang_code)
+        
+        # Determine preferred language
+        preferred_language = 'en'  # default
+        supported_languages = ['en', 'fr', 'es']  # your supported languages
+        
+        for lang in languages:
+            if lang in supported_languages:
+                preferred_language = lang
+                break
+            # Check language codes without region
+            lang_code = lang.split('-')[0]
+            if lang_code in supported_languages:
+                preferred_language = lang_code
+                break
+        if preferred_language:
+            return 'about/{}/privacy-policy.html'.format(preferred_language.lower())
+        else:
+            return 'about/en/privacy-policy.html'
